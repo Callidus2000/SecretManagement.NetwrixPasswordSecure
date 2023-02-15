a4e18519980d0e1a6b8d9a522a07b02f385e02b9
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2023-02-15 16:33:53 +0100

    Function added for Registering the SecretVault

--- SecretManagement.NetwrixPasswordSecure/functions/Register-NetwrixSecureVault.ps1
+++ SecretManagement.NetwrixPasswordSecure/functions/Register-NetwrixSecureVault.ps1
@@ -1,27 +1,50 @@
 ﻿function Register-NetwrixSecureVault {
+    <#
+    .SYNOPSIS
+    Registers a new Secretvault.
+
+    .DESCRIPTION
+    Registers a new Secretvault.
+
+    .PARAMETER VaultName
+    The name of the vault.
+
+    .PARAMETER Server
+    The hostname of the server which provides the service.
+
+    .PARAMETER Port
+    On which port is the server listening? Defaults to 11016.
+
+    .PARAMETER Database
+    The name of the Database.
+
+    .PARAMETER UserName
+    The username which is used to connect to the server. Has to be specified on vault level as Unlock-SecretVault only accepts a password instead credentials.
+
+    .EXAMPLE
+    Register-NetwrixSecureVault -VaultName myVault -Server myserver -Database PWDB -UserName fred
+
+    Registers the given vault.
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory)]
         [string]$VaultName,
-        [string]$Hostname,
+        [Parameter(Mandatory)]
+        [string]$Server,
         [string]$Port="11016",
+        [Parameter(Mandatory)]
         [string]$Database,
-        [string]$UserName,
-
-        #Don't validate the vault operation upon registration. This is useful for pre-staging
-        #vaults or vault configurations in deployments.
-        [Parameter(ParameterSetName = 'SkipValidate')][Switch]$SkipValidate,
-        [switch]$AllowClobber
+        [Parameter(Mandatory)]
+        [string]$UserName
     )
-
-    begin {
-
-    }
-
-    process {
-
-    }
-
-    end {
-
-    }
+    $myModulePath = "$ModuleRoot\SecretManagement.NetwrixPasswordSecure.psd1"
+    $psdData=Import-PowerShellDataFile $myModulePath
+    $additionalParameter = $PSBoundParameters | ConvertTo-PSFHashtable -Include "database","port","userName" ,"server" -Inherit
+    $additionalParameter.version=$psdData.ModuleVersion
+    Write-PSFMessage "Registering Vault $vault with Param $($additionalParameter|ConvertTo-Json -Compress) and Module $myModulePath, Version $($psdData.ModuleVersion)"
+    Register-SecretVault -Name $vaultName -ModuleName $myModulePath -VaultParameters $additionalParameter
 }
