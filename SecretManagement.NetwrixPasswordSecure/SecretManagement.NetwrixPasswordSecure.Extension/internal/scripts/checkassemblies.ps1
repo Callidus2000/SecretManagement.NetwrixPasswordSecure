44eb5a6e48ed161c3a3700a5ac3749092e824141
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2023-02-17 15:32:23 +0100

    The assemblies will not only be searched from with the current module but from previous versions, too.

--- SecretManagement.NetwrixPasswordSecure/SecretManagement.NetwrixPasswordSecure.Extension/internal/scripts/checkassemblies.ps1
+++ SecretManagement.NetwrixPasswordSecure/SecretManagement.NetwrixPasswordSecure.Extension/internal/scripts/checkassemblies.ps1
@@ -1,7 +1,14 @@
 ﻿Write-PSFMessage "Checking assemblies in `$moduleRoot=$moduleRoot"
+
 if (Test-Path "$moduleRoot\bin\PsrApi.dll") {
-    Add-Type -Path "$moduleRoot\bin\\PsrApi.dll"
-    Add-Type -Path "$moduleRoot\bin\Newtonsoft.Json.dll"
+    $dllPath = "$moduleRoot\bin\"
+}else{
+    $dllPath = get-module secretmanagement.netwrixpasswordsecure -ListAvailable | Select-Object -ExpandProperty path | Split-Path | Join-Path -ChildPath "SecretManagement.NetwrixPasswordSecure.Extension\bin" | Where-Object { test-path "$_\psrapi.dll" } | Sort-Object -Descending | Select-Object -first 1
+}
+if ($dllPath) {
+    Write-PSFMessage "Found the neccessary DLLs in the following folder: $dllPath"
+    Add-Type -Path "$dllPath\PsrApi.dll"
+    Add-Type -Path "$dllPath\Newtonsoft.Json.dll"
 }
 else {
     $errMessage = "Required Assemblies (like PsrAPI.dll) are missing in the folder '$moduleRoot\bin'. They are provided for *Enterprise* cutomers by the product vendor on request. Please unzip all provided *.DLL within this folder"
