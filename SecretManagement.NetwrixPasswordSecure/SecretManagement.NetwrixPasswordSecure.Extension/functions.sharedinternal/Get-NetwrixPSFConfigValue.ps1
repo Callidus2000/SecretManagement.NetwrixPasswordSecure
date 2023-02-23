34c57bedb88b05ab3549c834b587de0eac372eba
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2023-02-23 16:10:19 +0100

    Added helper for easier access to PSFConfig

---
+++ SecretManagement.NetwrixPasswordSecure/SecretManagement.NetwrixPasswordSecure.Extension/functions.sharedinternal/Get-NetwrixPSFConfigValue.ps1
@@ -0,0 +1,13 @@
+﻿function Get-NetwrixPSFConfigValue {
+    [CmdletBinding()]
+    param (
+        [hashtable]$AdditionalParameters,
+        [String]$SubPath
+    )
+    $AdditionalParameters = @{} + $AdditionalParameters
+    $configName = "SecretManagement.NetwrixPasswordSecure.Extension.$($AdditionalParameters.server -replace '\.','_').$($AdditionalParameters.Database).$SubPath"
+    Write-PSFMessage "Query PSFConfig $configName"
+    $value = Get-PSFConfigValue -FullName $configName
+    if(-not $value){Write-PSFMessage "No value found"}
+    return $value
+}
---
+++ SecretManagement.NetwrixPasswordSecure/SecretManagement.NetwrixPasswordSecure.Extension/functions.sharedinternal/Set-NetwrixPSFConfigValue.ps1
@@ -0,0 +1,20 @@
+﻿function Set-NetwrixPSFConfigValue {
+    [CmdletBinding()]
+    param (
+        [hashtable]$AdditionalParameters,
+        [String]$SubPath,
+        $Value,
+        [switch]$Initialize
+    )
+    $AdditionalParameters = @{} + $AdditionalParameters
+
+    $confParam=@{
+        Module      = "SecretManagement.NetwrixPasswordSecure.Extension"
+        Name        = "$($AdditionalParameters.server -replace '\.','_').$($AdditionalParameters.Database).$SubPath"
+        AllowDelete=$true
+        Initialize=$Initialize
+        Value=$Value
+    }
+    Write-PSFMessage "Saving PSFConfig: $($confParam|ConvertTo-Json -Compress -Depth 2)"
+    Set-PSFConfig @confParam
+}
